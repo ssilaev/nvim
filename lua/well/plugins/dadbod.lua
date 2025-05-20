f61d9ef273316779e73f699170a580b4ef368d74
Sergey Silaev <hello@8am.dev>
2025-05-20 11:23:49 +0200

    Clean up lua/well/plugins/dadbod.lua

--- lua/well/plugins/dadbod.lua
+++ lua/well/plugins/dadbod.lua
@@ -1,12 +1,3 @@
--- local cmp = require("cmp")
---
--- cmp.setup.filetype({ "sql" }, {
--- 	sources = {
--- 		{ name = "vim-dadbod-completion" },
--- 		{ name = "biffer" },
--- 	},
--- })
---
 return {
 	"tpope/vim-dadbod",
 	"kristijanhusak/vim-dadbod-completion",
